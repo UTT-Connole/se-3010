1aa9293b103e407222750263dc9d99fb152043b3
Brayden Connole <brayden.connole@gmail.com>
2025-01-15 14:57:27 -0700

    in class beanie store

--- beanie-store/app/index.tsx
+++ beanie-store/app/index.tsx
@@ -1,23 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, ToastAndroid, Platform, Alert } from "react-native";
+import {useState, useEffect} from 'react'
 
 
-const storeName = "Beanie Store v1"
+const storeName = "Beanie Store"
 
-const Beanie = (props: any) => {
-  return(
-      <Text>My {props.color} Beanie</Text>
+const Beanie = ({ color, hank }: { color: string; hank?: string }) => {
+  return (
+    <Text>My {color} Beanie by {hank}</Text>
   )
 }
 
 const BeanieStore = () => {
 
+  const [version, setVersion] = useState(0)
+
+  useEffect(() => {
+    console.log('BeanieStore mounted')
+  }, [])
+
+  useEffect(() => {
+    console.log('the new version is ', version)
+    if (version > 10) {
+      ToastAndroid.show('Version is too high', ToastAndroid.LONG)
+      setVersion(0)
+    } else if (version < 0) {
+      setVersion(10)
+    }
+
+  }, [version])
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to {storeName}</Text>
-      <Beanie color="Red"/>
-      <Beanie color="Blue"/>
-      <Beanie color="Something"/>
-      <Beanie color="Red"/>
+      <Text>Welcome to {storeName} v{version}</Text>
+      <Beanie color="Red" hank="Aaron"/>
+      <Beanie color="Blue" hank="Green"/>
+      <Beanie color="Nick" hank="Smith"/>
+      <Beanie color="Brayden"/>
+      <Button
+        onPress={() => setVersion(version + 1)}
+        title="Button"
+        color="#841584"
+      />
+
     </View>
   );
 }
